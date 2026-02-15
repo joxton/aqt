3cce9371705e374cbd4f3bd386575b2335a94334
joxton <62476366+joxton@users.noreply.github.com>
2026-02-15 14:08:11 +0100

    Delete src directory

--- src/aqt.h
+++
@@ -1,8 +0,0 @@
-
-
-double quantile_srtd(double *x, int n, double tau);
-int compare_doubles(const void *a, const void *b);
-
-void permute(double *z, double *x, int n, double *y, int m);
-void quantile_sorted(double *x, int *n, double *tau, int *ntau, double *q);
-int sample_indx(int n);
